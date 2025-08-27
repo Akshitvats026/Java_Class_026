f08c428b7ee69db1e405218538aa8e0c086b40ea
Akshit vats <akshitvats026@gmail.com>
2025-08-27 09:45:03 +0530

    Add files via upload

---
+++ Java_Classwork_026/src/Calculator/Exception.java
@@ -0,0 +1,23 @@
+package Calculator;
+
+import java.util.Scanner;
+
+public class Exception {
+    public static void main(String[] args) {
+        int a, b, z = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Two Integer: ");
+        a = sc.nextInt();
+        b = sc.nextInt();
+        try {
+            z = a / b;
+        }
+//        the thing too learn in exception is that catch cant  be above the try
+        catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            System.out.println(z);
+        }
+    }
+}
